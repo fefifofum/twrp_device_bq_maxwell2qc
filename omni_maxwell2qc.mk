c4081fac34ac8269aed2200f013c99b0be01e3a5
fefifofum <jorsellis@gmail.com>
2014-05-24 19:39:19 +0200

    Adapt to Omni

--- omni_maxwell2qc.mk
+++ omni_maxwell2qc.mk
@@ -1,9 +1,12 @@
+# Inherit some common Omni stuff
+$(call inherit-product, vendor/omni/config/common.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/bq/maxwell2qc/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := maxwell2qc
-PRODUCT_NAME := cm_maxwell2qc
+PRODUCT_NAME := omni_maxwell2qc
 PRODUCT_BRAND := bq
 PRODUCT_MANUFACTURER := bq
 PRODUCT_MODEL := bq Maxwell 2 Quad Core
